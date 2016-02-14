6c47923ad5a66f8c2a8109c1c05cb71e42817e07
Subigya Nepal <subigya_0324@deerwalk.edu.np>
2016-02-14 21:13:45 +0545

    new design

--- js/display.js
+++ js/display.js
@@ -15,6 +15,7 @@ $(function () {
     var  dateSelected;
     var bigCounter=0;
     	function init(){
+//console.log('Test');
 		var public_spreadsheet_url='https://docs.google.com/spreadsheets/d/1--POrwu6Lom3stoNe5LQz_BhyAb4Nz8-7ccRIpix4yM/pubhtml';
 		    tabletop = Tabletop.init( { key: public_spreadsheet_url,
                 callback: showInfo
@@ -23,10 +24,10 @@ $(function () {
 
            function cb(start, end) {
             $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-                console.log(start.format('DD/MM/YYYY') + ' ' + end.format('DD/MM/YYYY'));
+               // console.log(start.format('DD/MM/YYYY') + ' ' + end.format('DD/MM/YYYY'));
                 initialDateSelected=Date.parse(start.format('YYYY-MM-DD'));
                 finalDateSelected=Date.parse(end.format('YYYY-MM-DD'));
-                console.log(typeof initialDateSelected);                
+             //   console.log(typeof initialDateSelected);                
                 }
                 
                cb(moment().subtract(29, 'days'), moment());
@@ -45,22 +46,30 @@ $(function () {
         chart: {
             renderTo: 'container',
             type: 'line'
-         }
+         },
+	title:{
+	    text: ''
+	}
     });
     }
     
-      function createPieChart(counter){
+      function createPieChart(counter, companyName){
           pieCharts = new Highcharts.Chart({
         chart: {
             renderTo: 'container'+counter,
             type: 'pie'
-         }
+         }, plotOptions: {
+            pie: {
+                size: 150
+            }
+        }
              
         });
-          
+          pieCharts.setTitle({text: String(companyName)});
     }
     var submitCounter=0;
 	$("#submit").click(function(){ 
+
 		selectedCompanies = $('.selectpicker').val()	
 		if (selectedCompanies != null){		
             createLineChart();
@@ -72,10 +81,11 @@ $(function () {
             pieCharts.destroy();
             }
             
-            
+            $('#countrylink').show();
             $.each(selectedCompanies, function(index, value){
                 loadContent(this);
 			});
+
 		}
 	}); 
 
@@ -84,6 +94,7 @@ $(function () {
 			company[this] = tabletop.sheets(this).all();
           
 	});
+	$( "#submit" ).prop( "disabled", false );
 	}
     
     var initialDateToShow=function(){
@@ -103,7 +114,7 @@ $(function () {
         counter=0;
         bigCounter=bigCounter+1;
        
-        createPieChart(bigCounter);
+        createPieChart(bigCounter, companyName);
         var info=[];  // list of date and likes
 		var countryInfo=[]; // list of country and its respective likes
 		var data = company[companyName];
@@ -136,7 +147,7 @@ $(function () {
 					}
 				}
 			     
-				goInChart(info,countryInfo);//info is for line graph while countryInfo is for pie chart
+				goInChart(info,countryInfo, companyName);//info is for line graph while countryInfo is for pie chart
 		}
 	
     function returnDate(oldDate){
@@ -151,7 +162,7 @@ $(function () {
     
     
 
-	function goInChart(info,countryInfo){ // function to display both line graph and pie chart
+	function goInChart(info,countryInfo, companyName){ // function to display both line graph and pie chart
          categoriesData=[]; // dates are put here as a list
         lineData = [];
         if (categoriesData.length < 1)
@@ -166,15 +177,15 @@ $(function () {
              lineData.push(Number(info[i].likes));
         }      
               lineCharts.addSeries({
-        name: companyName,
-        type: 'line',
-        color: '#FFA500',  
+       name: String(companyName),
+        type: 'line', 
         data: lineData
     });
         
-    
+   // center: ['80%', '50px'],
         pieCharts.addSeries({
-            name: 'Total Consumption',
+	    name: String(companyName),
+            size: 150,
             data: countryInfo
         });
     
